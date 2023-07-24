4f3b4985f4a8b088caea1797f938a53973b5d79b
Tomokou <bychckova.margo@yandex.ru>
2023-07-24 20:11:46 +0300

    release

--- src/main/java/com/example/shelter_bot/ShelterBotApplication.java
+++ src/main/java/com/example/shelter_bot/ShelterBotApplication.java
@@ -2,7 +2,7 @@ package com.example.shelter_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//ветка с полным рабочим функционалом проекта
 @SpringBootApplication
 public class ShelterBotApplication {
 
