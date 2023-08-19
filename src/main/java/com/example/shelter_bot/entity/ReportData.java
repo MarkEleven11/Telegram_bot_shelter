4fac380572b03d1f90bca560d8027b2415e155cb
Ирина Капкова <kapiral@rambler.ru>
2023-08-19 19:59:49 +0400

    доавила отчет как класс

---
+++ src/main/java/com/example/shelter_bot/entity/ReportData.java
@@ -0,0 +1,42 @@
+package com.example.shelter_bot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Lob;
+import java.util.Date;
+
+/**
+ * Отчеты
+ **/
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+
+public class ReportData {
+    @GeneratedValue
+    @Id
+    private long id;
+    private Long chatId;
+    private String name;
+    private String health;
+    private String ration;
+    private String behaviour;
+    private Date lastMessage;
+    @Lob
+    private byte[] data;
+
+    public ReportData(Long chatId, String name, String health, String ration, String behaviour, Date lastMessage, byte[] data) {
+        this.chatId = chatId;
+        this.name = name;
+        this.health = health;
+        this.ration = ration;
+        this.behaviour = behaviour;
+        this.lastMessage = lastMessage;
+        this.data = data;
+    }
+}
