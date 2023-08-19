caa26178a6d53d44c9536e3d3bf25256f852a5c5
tatiana <tatiana.tokmantseva@gmail.com>
2023-08-19 17:16:00 +0500

    Добавлены методы работы с сообщениями

--- src/main/java/com/example/shelter_bot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/shelter_bot/listener/TelegramBotUpdatesListener.java
@@ -5,21 +5,30 @@ import com.example.shelter_bot.enums.PetType;
 import com.example.shelter_bot.entity.Shelter;
 import com.example.shelter_bot.service.*;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.*;
+import com.pengrad.telegrambot.model.File;
+import com.pengrad.telegrambot.model.PhotoSize;
+import com.pengrad.telegrambot.request.ForwardMessage;
+import com.pengrad.telegrambot.request.GetFile;
+import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.example.shelter_bot.entity.User;
+import com.pengrad.telegrambot.response.GetFileResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import com.example.shelter_bot.service.StartService;
 import com.example.shelter_bot.service.SendMessageService;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,6 +54,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     final Pattern pattern = Pattern.compile("(\"\\D+\")\\s+(\"\\d{10,11}\")");
     boolean contactUserFlag;
     boolean contactClientFlag;
+
+    private final ReportDataService reportDataService;
+
+    @Value("${volunteer-chat-id}")
+    private Long volunteerChatId;
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot, UserService userService,
                                       ShelterService shelterService, VolunteerService volunteerService,
@@ -88,81 +102,166 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         String text = message.text();
         SendResponse sendResponse;
         logger.info("Processing update: {}", update);
-            try {
-                switch (text) {
-                    case "/start" -> this.telegramBot.execute(startService.start(id));
-                    case "/catShelter" -> {
-                        clientIdToShelter.put(id, shelterService.chooseShelter(PetType.CAT));
-                        telegramBot.execute(shelterService.giveMenu(id));
-                    }
-                    case "/dogShelter" -> {
-                        clientIdToShelter.put(id, shelterService.chooseShelter(PetType.DOG));
-                        telegramBot.execute(shelterService.giveMenu(id));
-                    }
-                    case "/stage" -> {
-                        telegramBot.execute(shelterService.start(clientIdToShelter.get(id), id));
-                    }
-
-                    case "/about" -> sendResponse = clientIdToShelter.containsKey(id) ? telegramBot
-                            .execute(shelterService.aboutShelter(clientIdToShelter.get(id), id)) :
-                            telegramBot.execute(sendMessageService.shelterNotChoose(id));
-                    case "/info" -> sendResponse = clientIdToShelter.containsKey(id) ?
-                            telegramBot.execute(shelterService.infoShelter(clientIdToShelter.get(id), id)) :
-                            telegramBot.execute(sendMessageService.shelterNotChoose(id));
-
-                    case "/guard" -> sendResponse = clientIdToShelter.containsKey(id) ?
-                            telegramBot.execute(shelterService.getGuardContact(clientIdToShelter.get(id), id)) :
-                            telegramBot.execute(sendMessageService.shelterNotChoose(id));
-                    case "/contact" -> {
-                        if (clientIdToShelter.containsKey(id)) {
-                            telegramBot.execute(sendMessageService.send(id,
-                                    "Введите контактные данные в формате: \"Фамилия Имя Отчество\" " +
-                                            "\"номер телефона\".\nК примеру: \"Иванов Иван Иванович\" \"89990001122\""));
-                            contactUserFlag = true;
-                        } else {
-                            contactUserFlag = false;
-                        }
-                    }
-
-                    case "/volunteer" -> {
-                        sendResponse = clientIdToShelter.containsKey(id) ?
-                                telegramBot.execute(volunteerService.callVolunteer(id, clientIdToShelter.get(id))) :
-                                telegramBot.execute(sendMessageService.shelterNotChoose(id));
-                    }
-
-                    case "/giveDataToBot" -> {
-                        if (clientIdToShelter.containsKey(id)) {
-                            telegramBot.execute(sendMessageService.send(id,
-                                    "Отправь боту свои фамилию, имя, телефонный номер (из 11 цифр) " +
-                                            "и адрес в следующем формате: " +
-                                            "Иванов Иван 89991234567 Москва, ул. Дальняя, д.5, кв. 10 "));
-                            contactClientFlag = true;
-                        } else {
-                            contactClientFlag = false;
-                        }
-                    }
-
-                    default -> {
-                        Matcher matcher = pattern.matcher((CharSequence) message);
-
-                        if (contactUserFlag && matcher.find()) {
-                            User user = new User(matcher.group(1).replace("\"", ""),
-                                    matcher.group(2).replace("\"", ""), clientIdToShelter.get(id));
-                            userService.writeContact(user);
-                            telegramBot.execute(sendMessageService.send(id, "Спасибо! С Вами свяжутся"));
-                        } else if (contactClientFlag) {
-                            Client client = clientService.createClient(new Client());
-                            //clientService.createClient(client);
-                            telegramBot.execute(sendMessageService.send(id, "Спасибо! С Вами свяжутся"));
-                        } else {
-                            telegramBot.execute(sendMessageService.commandIncorrect(id));
-                        }
-                    }
-
-
+        try {
+            switch (text) {
+                case "/start" -> this.telegramBot.execute(startService.start(id));
+                case "/catShelter" -> {
+                    clientIdToShelter.put(id, shelterService.chooseShelter(PetType.CAT));
+                    telegramBot.execute(shelterService.giveMenu(id));
                 }
-            } catch (Exception e) {
-                logger.error(e.getMessage(), e);
+                case "/dogShelter" -> {
+                    clientIdToShelter.put(id, shelterService.chooseShelter(PetType.DOG));
+                    telegramBot.execute(shelterService.giveMenu(id));
+                }
+                case "/stage" -> {
+                    telegramBot.execute(shelterService.start(clientIdToShelter.get(id), id));
+                }
+
+                case "/about" -> sendResponse = clientIdToShelter.containsKey(id) ? telegramBot
+                        .execute(shelterService.aboutShelter(clientIdToShelter.get(id), id)) :
+                        telegramBot.execute(sendMessageService.shelterNotChoose(id));
+                case "/info" -> sendResponse = clientIdToShelter.containsKey(id) ?
+                        telegramBot.execute(shelterService.infoShelter(clientIdToShelter.get(id), id)) :
+                        telegramBot.execute(sendMessageService.shelterNotChoose(id));
+
+                case "/guard" -> sendResponse = clientIdToShelter.containsKey(id) ?
+                        telegramBot.execute(shelterService.getGuardContact(clientIdToShelter.get(id), id)) :
+                        telegramBot.execute(sendMessageService.shelterNotChoose(id));
+                case "/contact" -> {
+                    if (clientIdToShelter.containsKey(id)) {
+                        telegramBot.execute(sendMessageService.send(id,
+                                "Введите контактные данные в формате: \"Фамилия Имя Отчество\" " +
+                                        "\"номер телефона\".\nК примеру: \"Иванов Иван Иванович\" \"89990001122\""));
+                        contactUserFlag = true;
+                    } else {
+                        contactUserFlag = false;
+                    }
+                }
+
+                case "/volunteer" -> {
+                    sendResponse = clientIdToShelter.containsKey(id) ?
+                            telegramBot.execute(volunteerService.callVolunteer(id, clientIdToShelter.get(id))) :
+                            telegramBot.execute(sendMessageService.shelterNotChoose(id));
+                }
+
+                case "/giveDataToBot" -> {
+                    if (clientIdToShelter.containsKey(id)) {
+                        telegramBot.execute(sendMessageService.send(id,
+                                "Отправь боту свои фамилию, имя, телефонный номер (из 11 цифр) " +
+                                        "и адрес в следующем формате: " +
+                                        "Иванов Иван 89991234567 Москва, ул. Дальняя, д.5, кв. 10 "));
+                        contactClientFlag = true;
+                    } else {
+                        contactClientFlag = false;
+                    }
+                }
+
+                default -> {
+                    Matcher matcher = pattern.matcher((CharSequence) message);
+
+                    if (contactUserFlag && matcher.find()) {
+                        User user = new User(matcher.group(1).replace("\"", ""),
+                                matcher.group(2).replace("\"", ""), clientIdToShelter.get(id));
+                        userService.writeContact(user);
+                        telegramBot.execute(sendMessageService.send(id, "Спасибо! С Вами свяжутся"));
+                    } else if (contactClientFlag) {
+                        Client client = clientService.createClient(new Client());
+                        //clientService.createClient(client);
+                        telegramBot.execute(sendMessageService.send(id, "Спасибо! С Вами свяжутся"));
+                    } else {
+                        telegramBot.execute(sendMessageService.commandIncorrect(id));
+                    }
+                }
+
+
             }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
+    }
+
+    /**
+     * Метод отправки текстовых сообщений.
+     *
+     * @param id
+     * @param text
+     */
+    public void sendResponseMessage(long id, String text) {
+        SendMessage sendMessage = new SendMessage(id, text);
+        SendResponse sendResponse = telegramBot.execute(sendMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error during sending message: {}", sendResponse.description());
+        }
+    }
+
+    /**
+     * Метод пересылки сообщения волонтеру
+     *
+     * @param chatId
+     * @param messageId
+     */
+    public void sendMessageToVolunteer(long chatId, int messageId) {
+        ForwardMessage forwardMessage = new ForwardMessage(volunteerChatId, chatId, messageId);
+        SendResponse sendResponse = telegramBot.execute(forwardMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error during sending message: {}", sendResponse.description());
+        }
+    }
+
+    /**
+     * Метод получения отчета и отправки его волонтеру
+     *
+     * @param message
+     */
+    public void getReport(Message message, String petName) {
+        PhotoSize photo = message.photo()[0];
+        String caption = message.caption();
+        Long chatId = message.chat().id();
+
+        List<String> captionMatcher = splitCaption(caption);
+
+        String ration = captionMatcher.get(0);
+        String health = captionMatcher.get(1);
+        String behaviour = captionMatcher.get(2);
+
+        GetFile getFile = new GetFile(photo.fileId());
+        GetFileResponse getFileResponse = telegramBot.execute(getFile);
+
+        try {
+            File file = getFileResponse.file();
+            byte[] fileContent = telegramBot.getFileContent(file);
+
+            long date = message.date();
+            Date lastMessage = new Date(date * 1000);
+            reportDataService.uploadReportData(
+                    chatId, petName, fileContent, ration,
+                    health, behaviour, lastMessage);
+            sendMessageToVolunteer(chatId, message.messageId());
+            sendResponseMessage(chatId, "Your report has been accepted!");
+        } catch (IOException e) {
+            System.out.println("Photo upload error!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * Метод для разбивки описания под фотографии для добавления полученного текста в отчет
+     *
+     * @param caption
+     * @return
+     */
+    private List<String> splitCaption(String caption) {
+        if (caption == null || caption.isBlank()) {
+            throw new IllegalArgumentException("The description under the photo should not be empty. Resend the report!");
+        }
+        Matcher matcher = pattern.matcher(caption);
+        if (matcher.find()) {
+            return new ArrayList<>(List.of(matcher.group(3), matcher.group(7), matcher.group(11)));
+        } else {
+            throw new IllegalArgumentException("Check the correct of the entered data and send the report again.");
+        }
+    }
+
 }
